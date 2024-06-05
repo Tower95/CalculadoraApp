d290f7e7acc2c50f7d8006fd46837ebfad77afd0
Ismael Torres <torrestmi3@gmail.com>
2024-06-05 16:48:43 -0700

    Funciones

--- src/CalculadoraApp.java
+++ src/CalculadoraApp.java
@@ -2,17 +2,120 @@ import java.util.Scanner;
 
 public class CalculadoraApp {
     public static void main(String[] args) {
-        System.out.println("**** Aplicacion Calculadora ****");
-        System.out.print("Proporciona valor operando1: ");
 
         Scanner consola = new Scanner(System.in);
+        Boolean ciclo = true;
+
+        while (ciclo){
+            try {
+                System.out.println("**** Aplicacion Calculadora ****");
+                printMenu();
+                int option = Integer.parseInt(consola.nextLine());
+
+                if (option == 5) {
+                    ciclo = false;
+                    System.out.println("Gracias por usar la calculadora \nAdios...");
+                    break;
+                }
+
+                switch (option) {
+                    case 1:
+                        suma();
+                        break;
+                    case 2:
+                        resta();
+                        break;
+                    case 3:
+                        multi();
+                        break;
+                    case 4:
+                        division();
+                        break;
+                    default:
+                        System.out.println("Opcion erronea");
+                        ciclo = false;
+                        break;
+                }
+            }
+            catch (Exception e){
+                System.out.println("Ocurrio un error " +e.getMessage());
+            }
+            waitEnter();
+        }
+
+    }
+
+    public static void printMenu(){
+        System.out.println("Ingrese una de las siguientes opciopnes:");
+        //MENU
+        System.out.println("1.- SUMA");
+        System.out.println("2.- RESTA");
+        System.out.println("3.- MULTIPLICACION");
+        System.out.println("4.- DIVISION");
+        System.out.println("5.- SALIR");
+        System.out.print("Cual opcion quiere: ");
+    }
+
+    public static void suma (){
+        Scanner consola = new Scanner(System.in);
+        System.out.print("Proporciona valor operando1: ");
+
         var operando1 = Integer.parseInt( consola.nextLine());
 
-        System.out.print("Proporciona valor operand2: ");
+        System.out.print("Proporciona valor operando2: ");
         var operando2 = Integer.parseInt( consola.nextLine());
 
         var resultado = operando1 + operando2;
 
         System.out.println("resultado: "+resultado);
+    }
+    public static void resta (){
+        Scanner consola = new Scanner(System.in);
+        System.out.print("Proporciona valor operando1: ");
+
+        var operando1 = Integer.parseInt( consola.nextLine());
+
+        System.out.print("Proporciona valor operando2: ");
+        var operando2 = Integer.parseInt( consola.nextLine());
+
+        var resultado = operando1 - operando2;
+
+        System.out.println("resultado: "+resultado);
+    }
+
+    public static void multi (){
+        Scanner consola = new Scanner(System.in);
+        System.out.print("Proporciona valor operando1: ");
+
+        var operando1 = Integer.parseInt( consola.nextLine());
+
+        System.out.print("Proporciona valor operando2: ");
+        var operando2 = Integer.parseInt( consola.nextLine());
+
+        var resultado = operando1 * operando2;
+
+        System.out.println("resultado: "+resultado);
+    }
+
+    public static void division (){
+        Scanner consola = new Scanner(System.in);
+        System.out.print("Proporciona valor operando1: ");
+
+        var operando1 = Integer.parseInt( consola.nextLine());
+
+        System.out.print("Proporciona valor operando2: ");
+        var operando2 = Integer.parseInt( consola.nextLine());
+
+        var resultado = operando1 / operando2;
+
+        System.out.println("resultado: "+resultado);
+    }
+
+    public static void waitEnter (){
+
+        Scanner consola = new Scanner(System.in);
+        System.out.println("Presiona Enter para continuar...");
+        consola.nextLine();
+
     }
 }
